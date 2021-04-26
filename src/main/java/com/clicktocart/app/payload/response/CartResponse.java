969f04ea5a870f6aa287e92a3cfb1fabf3d23d00
Dilmi101 <dnimasha971@gmail.com>
2021-04-26 22:23:04 +0530

    Cart details added

---
+++ src/main/java/com/clicktocart/app/payload/response/CartResponse.java
@@ -0,0 +1,32 @@
+package com.clicktocart.app.payload.response;
+
+public class CartResponse {
+
+    private String itemName;
+    private int qty;
+    private double totalPrice;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
---
+++ src/main/java/com/clicktocart/app/services/CartService.java
@@ -0,0 +1,71 @@
+package com.clicktocart.app.services;
+
+import com.clicktocart.app.model.CartRecords;
+import com.clicktocart.app.model.Item;
+import com.clicktocart.app.payload.response.CartResponse;
+import com.clicktocart.app.repository.CartRepository;
+import com.clicktocart.app.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class CartService {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    public HashMap<String, String> addToCart(CartRecords cart) {
+
+        HashMap<String, String> hm = new HashMap<>();
+
+        double totPrice = (cart.getPrice() * cart.getQuantity());
+        cart.setStatus("P");
+        cart.setPrice(totPrice);
+        CartRecords add = cartRepository.save(cart);
+
+        if(add != null){
+           hm.put("message", "Success");
+        }
+        return hm;
+    }
+
+    public List<CartResponse> getAllItemsInCart(int id) {
+        List<CartRecords> cart = cartRepository.getCartDetailsByCustId(id);
+        List<CartResponse> cartResponse = new ArrayList<CartResponse>();
+
+        for(CartRecords cartRes : cart){
+            CartResponse response = new CartResponse();
+            response.setQty(cartRes.getQuantity());
+            response.setTotalPrice(cartRes.getPrice());
+
+            Item existingItem = itemRepository.findById(cartRes.getItemId()).orElse(null);
+            response.setItemName(existingItem.getName());
+
+            cartResponse.add(response);
+        }
+        return cartResponse;
+    }
+
+    public List<CartRecords> getItemInCartById(int custId, int id) {
+        return cartRepository.getCartDetailsByCustIdAndId(custId, id);
+    }
+
+    public String deleteItemByID(int id) {
+        cartRepository.deleteById(id);
+        return "Item deleted";
+    }
+
+    public CartRecords updateItemById(CartRecords cart) {
+        CartRecords existingItem = cartRepository.findById(cart.getId()).orElse(null);
+        existingItem.setQuantity(cart.getQuantity());
+
+        return cartRepository.save(existingItem);
+    }
+}
