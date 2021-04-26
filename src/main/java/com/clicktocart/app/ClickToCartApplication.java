f1e54573c36922815bb3a40979ab123c8ae23686
SenuraJayadeva <senurajayadeva@gmail.com>
2021-04-26 08:37:16 +0530

    initial commit

---
+++ src/main/java/com/clicktocart/app/ClickToCartApplication.java
@@ -0,0 +1,13 @@
+package com.clicktocart.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickToCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickToCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clicktocart/app/ClickToCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clicktocart.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickToCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
